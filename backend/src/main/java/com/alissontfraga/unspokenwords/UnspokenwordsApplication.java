421c880226f7516c2e2288158afdc3acd6f027e2
alissontfraga <alissontfraga@gmail.com>
2025-12-05 09:59:59 -0300

    init: initial project setup

---
+++ backend/src/main/java/com/alissontfraga/unspokenwords/UnspokenwordsApplication.java
@@ -0,0 +1,13 @@
+package com.alissontfraga.unspokenwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnspokenwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnspokenwordsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alissontfraga/unspokenwords/UnspokenwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissontfraga.unspokenwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnspokenwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
