22638338447d571b94c680ac45c9699f1228c034
alissontfraga <alissontfraga@gmail.com>
2026-02-10 10:34:56 -0300

    refactor(repository): change User  Id type to Long

--- backend/src/main/java/com/alissontfraga/unspokenwords/repository/UserRepository.java
+++ backend/src/main/java/com/alissontfraga/unspokenwords/repository/UserRepository.java
@@ -4,9 +4,12 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.alissontfraga.unspokenwords.model.User;
+import com.alissontfraga.unspokenwords.entity.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
     boolean existsByUsername(String username);
 }
+
