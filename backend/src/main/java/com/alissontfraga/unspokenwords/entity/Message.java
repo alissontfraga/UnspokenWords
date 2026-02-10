0e069c42953560b4205f2760ce4a12855991af76
alissontfraga <alissontfraga@gmail.com>
2026-02-10 10:33:35 -0300

    refactor(domain): change Message id to Long and GenerationType to Identity

--- backend/src/main/java/com/alissontfraga/unspokenwords/entity/Message.java
+++ backend/src/main/java/com/alissontfraga/unspokenwords/entity/Message.java
@@ -1,20 +1,21 @@
-package com.alissontfraga.unspokenwords.model;
+package com.alissontfraga.unspokenwords.entity;
 
 import java.time.LocalDate;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,11 +30,11 @@ import lombok.Setter;
 public class Message {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(columnDefinition = "text", nullable = false)
-    @NotBlank
+    @Column(nullable = false, length = 80)
+    @NotNull
     private String content;
 
     @Column(nullable = false)
